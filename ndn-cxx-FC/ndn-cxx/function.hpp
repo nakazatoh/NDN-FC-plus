82dc3c047c502b7ec8637a0a6676f1c6b2c0f37a
nakazatoh <nakazato@waseda.jp>
2021-05-15 21:24:51 +0900

    ndn-cxx function update

--- ndn-cxx-FC/ndn-cxx/function.hpp
+++ ndn-cxx-FC/ndn-cxx/function.hpp
@@ -42,15 +42,7 @@ using PartialFunction = Function;
 class Function
 {
 public: // nested types
-  class Error : public name::Component::Error
-  {
-  public:
-    explicit
-    Error(const std::string& what)
-      : name::Component::Error(what)
-    {
-    }
-  };
+ using Error = name::Component::Error;
 
   using Component = name::Component;
   using component_container = std::vector<Component>;
@@ -99,18 +91,24 @@ public: // constructors, encoding, decoding
    */
   Function(std::string uri);
 
+  /** @brief Write URI representation of the name to the output stream
+   *  @sa https://named-data.net/doc/NDN-packet-spec/0.3/name.html#ndn-uri-scheme
+   */
+  void
+  toUri(std::ostream& os, name::UriFormat format = name::UriFormat::DEFAULT) const;
+
   /** @brief Get URI representation of the name
-   *  @return URI representation; "ndn:" scheme identifier is not included
-   *  @sa https://named-data.net/doc/ndn-tlv/name.html#ndn-uri-scheme
-   *  @note To print URI representation into a stream, it is more efficient to use ``os << name``.
+   *  @return URI representation; the "ndn:" scheme identifier is not included
+   *  @note To print URI representation into a stream, it is more efficient to use `os << name`.
+   *  @sa https://named-data.net/doc/NDN-packet-spec/0.3/name.html#ndn-uri-scheme
    */
   std::string
-  toUri() const;
+  toUri(name::UriFormat format = name::UriFormat::DEFAULT) const;
 
   /** @brief Check if this Function instance already has wire encoding
    */
   bool
-  hasWire() const
+  hasWire() const noexcept
   {
     return m_wire.hasWire();
   }
@@ -142,7 +140,7 @@ public: // constructors, encoding, decoding
 public: // access
   /** @brief Check if name is empty
    */
-  bool
+  NDN_CXX_NODISCARD bool
   empty() const
   {
     return m_wire.elements().empty();
@@ -164,7 +162,7 @@ public: // access
   get(ssize_t i) const
   {
     if (i < 0) {
-      i += size();
+      i += static_cast<ssize_t>(size());
     }
     return reinterpret_cast<const Component&>(m_wire.elements()[i]);
   }
@@ -249,6 +247,25 @@ public: // iterators
   }
 
 public: // modifiers
+  /** @brief Replace the component at the specified index.
+   *  @param i zero-based index of the component to replace;
+   *           if negative, it is interpreted as offset from the end of the name
+   *  @param component the new component to use as a replacement
+   *  @return a reference to this name, to allow chaining.
+   *  @warning No bounds checking is performed, using an out-of-range index is undefined behavior.
+   */
+  Function&
+  set(ssize_t i, const Component& component);
+
+  /** @brief Replace the component at the specified index.
+   *  @param i zero-based index of the component to replace;
+   *           if negative, it is interpreted as offset from the end of the name
+   *  @param component the new component to use as a replacement
+   *  @return a reference to this name, to allow chaining.
+   *  @warning No bounds checking is performed, using an out-of-range index is undefined behavior.
+   */
+  Function&
+  set(ssize_t i, Component&& component);
   /** @brief Append a component
    *  @return a reference to this name, to allow chaining
    */
@@ -259,17 +276,24 @@ public: // modifiers
     return *this;
   }
 
-  /** @brief Append a component, copying from a zero-terminated byte string
-   *  @param value a zero-terminated string; it will be constructed as a component as is
-   *               and will not be interpreted as a URI
-   *  @note This overload is necessary to ensure unambiguity of ``append("string")``, because both
-   *        Component and PartialFunction are implicitly convertible from zero-terminated byte string.
-   *        This overload ensures the string is constructed as a Component.
+  /** @brief Append a component.
+   *  @return a reference to this name, to allow chaining.
    */
   Function&
-  append(const char* value)
+  append(Component&& component)
   {
-    return append(Component(value));
+    m_wire.push_back(std::move(component));
+    return *this;
+  }
+
+  /** @brief Append a NameComponent of TLV-TYPE @p type, copying @p count bytes at @p value as
+   *         TLV-VALUE.
+   *  @return a reference to this name, to allow chaining.
+   */
+  Function&
+  append(uint32_t type, const uint8_t* value, size_t count)
+  {
+    return append(Component(type, value, count));
   }
 
   /** @brief Append a component, copying from [@p value, @p value + @p valueLength)
@@ -279,6 +303,21 @@ public: // modifiers
   append(const uint8_t* value, size_t valueLength)
   {
     return append(Component(value, valueLength));
+  }
+
+  /** @brief Append a NameComponent of TLV-TYPE @p type, copying TLV-VALUE from a range.
+   *  @tparam Iterator an @c InputIterator dereferencing to a one-octet value type. More efficient
+   *                   implementation is available when it is a @c RandomAccessIterator.
+   *  @param type      the TLV-TYPE.
+   *  @param first     beginning of the range.
+   *  @param last      past-end of the range.
+   *  @return a reference to this name, to allow chaining.
+   */
+  template<class Iterator>
+  Function&
+  append(uint32_t type, Iterator first, Iterator last)
+  {
+    return append(Component(type, first, last));
   }
 
   /** @brief Append a component, copying from [@p first, @p last)
@@ -293,9 +332,18 @@ public: // modifiers
   Function&
   append(Iterator first, Iterator last)
   {
-    static_assert(sizeof(typename std::iterator_traits<Iterator>::value_type) == 1,
-                  "iterator does not dereference to one-octet value type");
     return append(Component(first, last));
+  }
+
+  /** @brief Append a GenericNameComponent, copying TLV-VALUE from a null-terminated string.
+   *  @param str a null-terminated string. Bytes from the string are copied as is, and not
+   *             interpreted as URI component.
+   *  @return a reference to this name, to allow chaining.
+   */
+  Function&
+  append(const char* str)
+  {
+    return append(Component(str));
   }
 
   /** @brief Append a component, decoding from a Block
@@ -303,17 +351,24 @@ public: // modifiers
    *  @return a reference to this name, to allow chaining
    */
   Function&
-  append(const Block& value)
+  append(Block& value)
   {
     if (value.type() == tlv::GenericNameComponent) {
       m_wire.push_back(value);
     }
     else {
-      m_wire.push_back(Block(tlv::GenericNameComponent, value));
+      m_wire.push_back(Block(tlv::GenericNameComponent, std::move(value)));
     }
 
     return *this;
   }
+
+  /** @brief Append a PartialName.
+   *  @param name the components to append
+   *  @return a reference to this name, to allow chaining
+   */
+  Function&
+  append(const PartialFunction& name);
 
   /** @brief Append a component with a nonNegativeInteger
    *  @sa number the number
@@ -342,24 +397,13 @@ public: // modifiers
   }
 
   /** @brief Append a version component
+   *  @param version the version number to append; if nullopt, the current UNIX time
+   *                 in milliseconds is used
    *  @return a reference to this name, to allow chaining
    *  @sa NDN Naming Conventions https://named-data.net/doc/tech-memos/naming-conventions.pdf
    */
   Function&
-  appendVersion(uint64_t version)
-  {
-    return append(Component::fromVersion(version));
-  }
-
-  /** @brief Append a version component based on current time
-   *
-   *  The version number is the current UNIX timestamp in milliseconds
-   *
-   *  @return a reference to this name, to allow chaining
-   *  @sa NDN Naming Conventions https://named-data.net/doc/tech-memos/naming-conventions.pdf
-   */
-  Function&
-  appendVersion();
+  appendVersion(optional<uint64_t> version = nullopt);
 
   /** @brief Append a segment number (sequential) component
    *  @return a reference to this name, to allow chaining
@@ -382,21 +426,12 @@ public: // modifiers
   }
 
   /** @brief Append a timestamp component
+   *  @param timestamp the timestamp to append; if nullopt, the current system time is used
    *  @return a reference to this name, to allow chaining
    *  @sa NDN Naming Conventions https://named-data.net/doc/tech-memos/naming-conventions.pdf
    */
   Function&
-  appendTimestamp(const time::system_clock::TimePoint& timePoint)
-  {
-    return append(Component::fromTimestamp(timePoint));
-  }
-
-  /** @brief Append a timestamp component based on current time
-   *  @return a reference to this name, to allow chaining
-   *  @sa NDN Naming Conventions https://named-data.net/doc/tech-memos/naming-conventions.pdf
-   */
-  Function&
-  appendTimestamp();
+  appendTimestamp(optional<time::system_clock::TimePoint> timestamp = nullopt);
 
   /** @brief Append a sequence number component
    *  @return a reference to this name, to allow chaining
@@ -412,9 +447,9 @@ public: // modifiers
    *  @return a reference to this name, to allow chaining
    */
   Function&
-  appendImplicitSha256Digest(const ConstBufferPtr& digest)
+  appendImplicitSha256Digest(ConstBufferPtr& digest)
   {
-    return append(Component::fromImplicitSha256Digest(digest));
+    return append(Component::fromImplicitSha256Digest(std::move(digest)));
   }
 
   /** @brief Append an ImplicitSha256Digest component
@@ -426,15 +461,32 @@ public: // modifiers
     return append(Component::fromImplicitSha256Digest(digest, digestSize));
   }
 
-  /** @brief Append a PartialFunction
-   *  @param name the components to append
+  /** @brief Append a ParametersSha256Digest component.
    *  @return a reference to this name, to allow chaining
    */
   Function&
-  append(const PartialFunction& name);
+  appendParametersSha256Digest(ConstBufferPtr digest)
+  {
+    return append(Component::fromParametersSha256Digest(std::move(digest)));
+  }
+
+  /** @brief Append a ParametersSha256Digest component.
+   *  @return a reference to this name, to allow chaining
+   */
+  Function&
+  appendParametersSha256Digest(const uint8_t* digest, size_t digestSize)
+  {
+    return append(Component::fromParametersSha256Digest(digest, digestSize));
+  }
+
+  /** @brief Append a placeholder for a ParametersSha256Digest component.
+   *  @return a reference to this name, to allow chaining
+   */
+  Function&
+  appendParametersSha256DigestPlaceholder();
 
   /** @brief Append a component
-   *  @note This makes push_back an alias of append, giving Function a similar API as STL vector.
+   *  @note This makes push_back an alias of append, giving Name a similar API as STL vector.
    */
   template<class T>
   void
@@ -443,14 +495,19 @@ public: // modifiers
     append(component);
   }
 
+  /** @brief Erase the component at the specified index.
+   *  @param i zero-based index of the component to replace;
+   *           if negative, it is interpreted as offset from the end of the name
+   *  @warning No bounds checking is performed, using an out-of-range index is undefined behavior.
+   */
+  void
+  erase(ssize_t i);
+
   /** @brief Remove all components
    *  @post empty() == true
    */
   void
-  clear()
-  {
-    m_wire = Block(tlv::Function);
-  }
+  clear();
 
 public: // algorithms
   /** @brief Get the successor of a name
@@ -530,6 +587,55 @@ public: // algorithms
   int
   compare(size_t pos1, size_t count1,
           const Function& other, size_t pos2 = 0, size_t count2 = npos) const;
+private: // non-member operators
+  // NOTE: the following "hidden friend" operators are available via
+  //       argument-dependent lookup only and must be defined inline.
+
+  friend bool
+  operator==(const Function& lhs, const Function& rhs)
+  {
+    return lhs.equals(rhs);
+  }
+
+  friend bool
+  operator!=(const Function& lhs, const Function& rhs)
+  {
+    return !lhs.equals(rhs);
+  }
+
+  friend bool
+  operator<(const Function& lhs, const Function& rhs)
+  {
+    return lhs.compare(rhs) < 0;
+  }
+
+  friend bool
+  operator<=(const Function& lhs, const Function& rhs)
+  {
+    return lhs.compare(rhs) <= 0;
+  }
+
+  friend bool
+  operator>(const Function& lhs, const Function& rhs)
+  {
+    return lhs.compare(rhs) > 0;
+  }
+
+  friend bool
+  operator>=(const Function& lhs, const Function& rhs)
+  {
+    return lhs.compare(rhs) >= 0;
+  }
+
+  /** @brief Print URI representation of a name
+   *  @sa https://named-data.net/doc/ndn-tlv/name.html#ndn-uri-scheme
+   */
+  friend std::ostream&
+  operator<<(std::ostream& os, const Function& name)
+  {
+    name.toUri(os);
+    return os;
+  }
 
 public:
   /** @brief indicates "until the end" in getSubFunction and compare
@@ -541,48 +647,6 @@ private:
 };
 
 NDN_CXX_DECLARE_WIRE_ENCODE_INSTANTIATIONS(Function);
-
-inline bool
-operator==(const Function& lhs, const Function& rhs)
-{
-  return lhs.equals(rhs);
-}
-
-inline bool
-operator!=(const Function& lhs, const Function& rhs)
-{
-  return !lhs.equals(rhs);
-}
-
-inline bool
-operator<=(const Function& lhs, const Function& rhs)
-{
-  return lhs.compare(rhs) <= 0;
-}
-
-inline bool
-operator<(const Function& lhs, const Function& rhs)
-{
-  return lhs.compare(rhs) < 0;
-}
-
-inline bool
-operator>=(const Function& lhs, const Function& rhs)
-{
-  return lhs.compare(rhs) >= 0;
-}
-
-inline bool
-operator>(const Function& lhs, const Function& rhs)
-{
-  return lhs.compare(rhs) > 0;
-}
-
-/** @brief Print URI representation of a name
- *  @sa https://named-data.net/doc/ndn-tlv/name.html#ndn-uri-scheme
- */
-std::ostream&
-operator<<(std::ostream& os, const Function& name);
 
 /** @brief Parse URI from stream as Function
  *  @sa https://named-data.net/doc/ndn-tlv/name.html#ndn-uri-scheme
